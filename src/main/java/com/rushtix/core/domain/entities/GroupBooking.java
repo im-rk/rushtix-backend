b1b18511242bb63c1da8d6a5717abe47d38c9f04
im-rk <krramkumar2006g@gmail.com>
2026-06-21 22:20:40 +0530

    feat : schema setup for group pay

---
+++ src/main/java/com/rushtix/core/domain/entities/GroupBooking.java
@@ -0,0 +1,40 @@
+package com.rushtix.core.domain.entities;
+
+import com.rushtix.core.domain.enums.GroupBookingStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name="group_bookings")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupBooking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID evenetId;
+    private UUID initiatorUserId;
+
+
+    private GroupBookingStatus status;
+
+    private BigDecimal totalAmount;
+    private BigDecimal perPersonAmount;
+
+    private OffsetDateTime expiresAt;
+    private OffsetDateTime createdAt;
+
+    @OneToMany(mappedBy = "groupBooking", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<GroupPaymentItem> paymentItems;
+}
---
+++ src/main/java/com/rushtix/core/domain/enums/GroupBookingStatus.java
@@ -0,0 +1,7 @@
+package com.rushtix.core.domain.enums;
+
+public enum GroupBookingStatus {
+    PENDING_GROUP_PAYMENT,
+    CONFIRMED,
+    FAILED_REJECTED
+}
